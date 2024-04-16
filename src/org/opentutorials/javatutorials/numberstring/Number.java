9cd3ab664e8b2b0798807ebd32f91fbc28c18ca4
hanjunnn <qwedfr79@naver.com>
2024-04-16 16:42:55 +0900

    숫자 실습

---
+++ src/org/opentutorials/javatutorials/numberstring/Number.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+    public static void main(String[] args) {    
+        //숫자와 문자
+        //자바는 따옴표가 없는 숫자는 숫자로 인식
+        // System.out.println(1+2);정수
+        // System.out.println(1.2+1.3);실수
+        // System.out.println(2*5);곱하기
+        // System.out.println(6/2);나누기
+    }
+}
