ab921706d9fabadbde2b32d5965ac35749befd70
hanjunnn <qwedfr79@naver.com>
2024-04-16 17:04:00 +0900

    문자와 문자열

---
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+    public static void main(String[] args) {    
+    //문자와 문자열
+        //문자는 한 글자를 의미, 문자열은 여러 개의 문자가 결합한 것을 의미
+        //문자는 작은 따옴표로 감싸야함
+        //문자열은 큰 따옴표로 감싸야함
+        //""앞에 ₩를 붙이면 큰 따옴표를 문자열로 인식
+        System.out.println("나는야 코딩천재"+"한준");
+    }
+}
