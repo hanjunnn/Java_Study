24758538bcacab41201588c6fb0d711b49756334
hanjunnn <qwedfr79@naver.com>
2024-04-16 20:16:24 +0900

    변수-1

---
+++ src/org/opentutorials/javatutorials/variables/VariablesDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.variables;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        //데이터형식 변수명;
+        //정수
+        int a;
+        a = 1;
+        System.out.println(a+1);//2
+        a = 2;
+        System.out.println(a+1);//3
+
+        //실수
+        double b;
+        b = 1.1;
+        System.out.println(b+1.1);//2.2
+    }
+}
