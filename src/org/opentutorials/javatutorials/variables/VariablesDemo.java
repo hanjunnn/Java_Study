fa0703e6ecc44d33f2d0622f0aad1704548577c5
hanjunnn <qwedfr79@naver.com>
2024-04-17 14:30:50 +0900

    상수형 데이터 타입

--- src/org/opentutorials/javatutorials/variables/VariablesDemo.java
+++ src/org/opentutorials/javatutorials/variables/VariablesDemo.java
@@ -17,14 +17,27 @@ public class VariablesDemo {
 
         // 문자열
         // String first = "coding";
-        String a, b;
-        a = "coding";
-        b = "everybody";
-        System.out.println(a+b);
+        // String a, b;
+        // a = "coding";
+        // b = "everybody";
+        // System.out.println(a+b);
 
         //변수를 사용하는 이유
         //중복의 제거
         //가독성
         //유지보수
+
+        //데이터 타입
+        //메모리의 소모는 데이터 타입에 따라 다름
+        //정수형
+        //byte, short, int, long 주로 int 사용
+        //실수형
+        //float, double 주로 double 사용
+        //문자형
+        //char
+        
+        //상수는 변하지 않는 값을 의미
+        long a = 2147483648L;//int의 범위를 넘어선 값을 대입하려면 뒤에 L을 붙여야함
+        byte b = 100;//byte의 범위를 넘어선 값을 대입하려면 형변환 필요
     }
 }
