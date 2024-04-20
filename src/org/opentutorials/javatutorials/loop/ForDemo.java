aa0f26466eafe81994561b5502f8ee7c9bffb8d5
hanjunnn <qwedfr79@naver.com>
2024-04-20 17:46:45 +0900

    break,continue

--- src/org/opentutorials/javatutorials/loop/ForDemo.java
+++ src/org/opentutorials/javatutorials/loop/ForDemo.java
@@ -4,6 +4,8 @@ public class ForDemo {
     public static void main(String[] args){
         for(int i = 0; i < 10; i++){
             System.out.println("Coding Everybody"+i);
+            //break는 반복문을 중단시키는 역할
+            //continue는 반복문을 중단시키지 않고 다음 반복으로 넘어가게 하는 역할
         }
     }
 }
