73b1b0099c9bc77debe37a8081052a5cb2b2a381
hanjunnn <qwedfr79@naver.com>
2024-04-20 16:43:04 +0900

    조건문 응용,중첩

---
+++ src/org/opentutorials/javatutorials/condition/LoginDemo.java
@@ -0,0 +1,23 @@
+package org.opentutorials.javatutorials.condition;
+
+public class LoginDemo {
+    public static void main(String[] args) {
+        // String id = args[0]; //입력값을 받아옴
+        // if(id.equals("egoing")){ //id가 egoing이면
+        //     System.out.println("right");
+        // } else {
+        //     System.out.println("wrong");
+        // }
+        String id = "egoing";
+        String password = "111111";
+        if (id.equals("egoing")) {
+            if (password.equals("111111")) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        } else {
+            System.out.println("wrong");
+        }
+    }
+}
