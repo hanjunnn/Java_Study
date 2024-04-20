2e59032632fca54e65c2d4da13172bd9739d4674
hanjunnn <qwedfr79@naver.com>
2024-04-20 17:13:32 +0900

    not연산자

---
+++ src/org/opentutorials/javatutorials/condition/NotDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.condition;
+
+public class NotDemo {
+    public static void main(String[] args){
+        if(!true){
+            System.out.println(1);
+        }
+        if(!false){
+            System.out.println(2);
+        }
+    //!은 부정을 의미 true에 !를 붙이면 false가 되고 false에 !를 붙이면 true가 된다.
+    }
+}
