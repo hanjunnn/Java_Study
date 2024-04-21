bc0430df928dfa5e251fae7abf1ce39555c433fd
hanjunnn <qwedfr79@naver.com>
2024-04-21 16:09:45 +0900

    앱이 실행중에 데이터 입력

---
+++ src/org/opentutorials/javatutorials/io/ScannerDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.io;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int i = sc.nextInt();
+    //     System.out.println(i*1000);
+    //     sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()) {
+            System.out.println(sc.nextInt()*1000);
+        }
+        sc.close();
+    }
+}
