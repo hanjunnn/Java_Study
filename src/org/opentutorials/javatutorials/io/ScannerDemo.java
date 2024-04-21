502e97c29cadcdc55eb9e8ae50af2e1b45332b61
hanjunnn <qwedfr79@naver.com>
2024-04-21 16:19:50 +0900

    파일로 입출력

--- src/org/opentutorials/javatutorials/io/ScannerDemo.java
+++ src/org/opentutorials/javatutorials/io/ScannerDemo.java
@@ -1,5 +1,7 @@
 package org.opentutorials.javatutorials.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ScannerDemo {
@@ -8,11 +10,23 @@ public class ScannerDemo {
     //     int i = sc.nextInt();
     //     System.out.println(i*1000);
     //     sc.close();
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     while(sc.hasNextInt()) {
+    //         System.out.println(sc.nextInt()*1000);
+    //     }
+    //     sc.close();
+    // }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()) {
-            System.out.println(sc.nextInt()*1000);
+        try {
+            File file = new File("out.txt");
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextInt()) {
+                System.out.println(sc.nextInt()*1000);
+            }
+            sc.close();
+        }catch(FileNotFoundException e) {
+            e.printStackTrace();
         }
-        sc.close();
     }
 }
