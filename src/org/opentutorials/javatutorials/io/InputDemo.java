cd4c2edb11243231af8c33cf83786c390d92e9ca
hanjunnn <qwedfr79@naver.com>
2024-04-21 15:32:35 +0900

    터미널에서 데이터 입력

---
+++ src/org/opentutorials/javatutorials/io/InputDemo.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.io;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+    }
+}
+//hanjun@hanjun-ui-MacBookPro Java1 % java -cp bin org.opentutorials.javatutorials.io.InputDemo one two three
+//3
