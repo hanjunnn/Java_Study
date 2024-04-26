7e62230ccb31f76e8ff41d990f61178ed765436b
hanjunnn <qwedfr79@naver.com>
2024-04-26 12:13:20 +0900

    객체화 주석추가

--- src/org/opentutorials/javatutorials/object/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/object/CalculatorDemo.java
@@ -1,6 +1,6 @@
 package org.opentutorials.javatutorials.object;
 
-class Calculator {
+class Calculator {//클래스
     int left, right;
     public void setOprands(int left, int right){
         this.left = left;
@@ -15,7 +15,9 @@ class Calculator {
 }
 public class CalculatorDemo {
     public static void main(String[] args) {
-        Calculator c1 = new Calculator();
+        Calculator c1 = new Calculator();//인스턴스화
+        //데이터타입 변수명 = new 데이터타입();
+        
         c1.setOprands(10, 20);
         c1.sum();
         c1.avg();
