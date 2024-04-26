fec3661841f02487d383b5c4b7ed708c4b4661e7
hanjunnn <qwedfr79@naver.com>
2024-04-26 13:14:53 +0900

    this

--- src/org/opentutorials/javatutorials/scope/ScopeDemo.java
+++ src/org/opentutorials/javatutorials/scope/ScopeDemo.java
@@ -1,6 +1,19 @@
 package org.opentutorials.javatutorials.scope;
+//절차지향 프로그래밍 에서는 모든 메소드에서 접근가능한 변수의 사용을 지양한다.
+class C{
+    int v = 10;
+    void m() {
+        int v = 20;
+        System.out.println(v);
+        System.out.println(this.v);
+    }
 
+}
 public class ScopeDemo {
+    public static void main(String[] args) {
+        C c1 = new C();
+        c1.m();
+    }
     // static void a() {
     //     int i = 0;//유효범위 scope
     // }
@@ -39,16 +52,17 @@ public class ScopeDemo {
     //     // System.out.println(i);
     //     // i는 for문에서 선언된 지역 변수이기 때문에 for문이 끝나면 사용할 수 없다.
     // }
-    static int i = 5;
-    static void a() {
-        int i = 10;
-        b();
-    }
-    static void b() {
-        int i = 20;
-        System.out.println(i);
-    }
-    public static void main(String[] args) {
-        a();
-    }
+    // static int i = 5;
+    // static void a() {
+    //     int i = 10;
+    //     b();
+    // }
+    // static void b() {
+    //     int i = 20;
+    //     System.out.println(i);
+    // }
+    // public static void main(String[] args) {
+    //     a();
+    // }
+
 }
