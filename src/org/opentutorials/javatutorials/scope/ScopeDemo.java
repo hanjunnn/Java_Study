425a3a1bd6a5dec5d8ce371c912e7fb21ada92c6
hanjunnn <qwedfr79@naver.com>
2024-04-26 12:57:08 +0900

    전역변수 지역변수

--- src/org/opentutorials/javatutorials/scope/ScopeDemo.java
+++ src/org/opentutorials/javatutorials/scope/ScopeDemo.java
@@ -1,14 +1,26 @@
 package org.opentutorials.javatutorials.scope;
 
 public class ScopeDemo {
+    // static void a() {
+    //     int i = 0;//유효범위 scope
+    // }
+    // public static void main(String[] args) {
+    //     for (int i = 0; i < 5; i++) {
+    //         a();
+    //         System.out.println(i);
+    //     }
+    // } 
+   
+    // // i는 a() 메소드에서 선언된 지역 변수이기 때문에 main() 메소드에서 사용할 수 없다.
+    static int i;//전역변수
     static void a() {
-        int i = 0;//유효범위 scope
+        i = 0;
     }
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (i = 0; i < 5; i++) {
             a();
             System.out.println(i);
         }
-    } 
-    // i는 a() 메소드에서 선언된 지역 변수이기 때문에 main() 메소드에서 사용할 수 없다.
+    }
+    //무한루프에 빠지게 된다.
 }
