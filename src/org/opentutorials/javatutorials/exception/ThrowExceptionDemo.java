080845d5b44f4329fd2acdd7e15108d98fe226f4
hanjunnn <qwedfr79@naver.com>
2024-05-01 13:25:02 +0900

    책임의 전가 throws

--- src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
+++ src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
@@ -1,17 +1,39 @@
 package org.opentutorials.javatutorials.exception;
-class B{
-    void run(){
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class B {
+    void run() throws FileNotFoundException, IOException {
+        BufferedReader bReader = null;
+        String input = null;
+        bReader = new BufferedReader(new java.io.FileReader("out.txt"));
+        input = bReader.readLine();
+
+        System.out.println(input);
     }
 }
-class C{
-    void run(){
-    B b = new B();
-    b.run();
+
+class C {
+    void run() throws FileNotFoundException, IOException {
+        B b = new B();
+        b.run();
     }
 }
+
 public class ThrowExceptionDemo {
     public static void main(String[] args) {
         C c = new C();
-        c.run();
+        try {
+            c.run();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            System.out.println("out.txt 파일이 필요합니다.");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
+//throw를 이용해서 메인 메소드에서 예외를 처리하도록 할 수 있다.
