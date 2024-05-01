4ac732a71bb6f27a38236c81e604311e83fe6d1e
hanjunnn <qwedfr79@naver.com>
2024-05-01 16:04:22 +0900

    사용자 정의 예외

--- src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -1,5 +1,19 @@
 package org.opentutorials.javatutorials.exception;
-
+class DivideException extends Exception {
+    public int left;
+    public int right;
+    DivideException() {
+        super();
+    }
+    DivideException(String message) {
+        super(message);
+    }
+    DivideException(String message, int left, int right) {
+        super(message);
+        this.left = left;
+        this.right = right;
+    }
+}
 class Calculator {
     int left, right;
 
@@ -11,32 +25,44 @@ class Calculator {
         this.right = right;
     }
 
-    public void divide() {
+    public void divide() throws DivideException {
+        // try{
+        //     if(this.right == 0) {
+        //         throw new DivideException("0으로 나누는 것은 허용되지 않습니다.");
+        //     }
+        //     System.out.print(this.left/this.right);
+        // } catch(DivideException e) {
+        //     e.printStackTrace();
+        // }
+
         if(this.right == 0) {
-            throw new ArithmeticException("0으로 나누는 것은 허용되지 않습니다.");
+            throw new DivideException("0으로 나누는 것은 허용되지 않습니다.",this.left,this.right);
         }
+        System.out.print(this.left/this.right);
+
+
         // 계산결과는 오류가 발생했습니다: / by zero
-        try {
-            System.out.print("계산결과는  ");
-            System.out.print(this.left / this.right);
-            System.out.print(" 입니다.");
-        } catch (Exception e) {
-            System.out.println("\n\ne.getMessage()\n" + e.getMessage());
-            // e.getMessage()
-            // / by zero
-            System.out.println("\n\ne.toString()\n" + e.toString());
-            // e.toString()
-            // java.lang.ArithmeticException: / by zero
-            System.out.println("\n\ne.printStackTrace()");
-            e.printStackTrace();
-            // e.printStackTrace()
-            // java.lang.ArithmeticException: / by zero
-            // at
-            // org.opentutorials.javatutorials.exception.Calculator.divide(CalculatorDemo.java:12)
-            // at
-            // org.opentutorials.javatutorials.exception.CalculatorDemo.main(CalculatorDemo.java:26)
-        }
-        System.out.println("Divide End");
+        // try {
+        //     System.out.print("계산결과는  ");
+        //     System.out.print(this.left / this.right);
+        //     System.out.print(" 입니다.");
+        // } catch (Exception e) {
+        //     System.out.println("\n\ne.getMessage()\n" + e.getMessage());
+        //     // e.getMessage()
+        //     // / by zero
+        //     System.out.println("\n\ne.toString()\n" + e.toString());
+        //     // e.toString()
+        //     // java.lang.ArithmeticException: / by zero
+        //     System.out.println("\n\ne.printStackTrace()");
+        //     e.printStackTrace();
+        //     // e.printStackTrace()
+        //     // java.lang.ArithmeticException: / by zero
+        //     // at
+        //     // org.opentutorials.javatutorials.exception.Calculator.divide(CalculatorDemo.java:12)
+        //     // at
+        //     // org.opentutorials.javatutorials.exception.CalculatorDemo.main(CalculatorDemo.java:26)
+        // }
+        // System.out.println("Divide End");
     }
 }
 
@@ -46,8 +72,10 @@ public class CalculatorDemo {
         c1.setOprands(10, 0);
         try {
             c1.divide();
-        } catch (ArithmeticException e) {
+        } catch (DivideException e) {
             System.out.println(e.getMessage());
+            System.out.println(e.left);
+            System.out.println(e.right);
         }
     }
 }
